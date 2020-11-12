0d220d15ea2418b70a9867581ad5adfbbaff1655
Toomas Vahter <toomas.vahter@gmail.com>
2020-11-12 19:15:10 +0900

    Add tvOS and watchOS to supported platforms.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,9 @@ let package = Package(
     name: "IndexedDataStore",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v5)
     ],
     products: [
         .library(
